5195dccb54ca88ded0e97e51ff032ecc1bd494ed
Nicolas Hel <znirgal@gmail.com>
2016-06-14 14:21:53 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 
 date_default_timezone_set('UTC');
 
-$mysqli = new mysqli("localhost", "root", "momo", "todo");
+$mysqli = new mysqli("localhost", "root", "", "todo");
 if ($mysqli->connect_errno) {
     echo "Echec connexion a MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     exit();
